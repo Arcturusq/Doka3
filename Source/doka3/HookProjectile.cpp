8b4c614d58336be2c3c6236d3bab3af0eda004ad
Arcturusq <arcturusq@yandex.ru>
2024-07-18 13:02:42 +0300

    Hookprojectile

---
+++ Source/doka3/HookProjectile.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HookProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AHookProjectile::AHookProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	CollisionComponent->InitSphereRadius(15.0f);
+	CollisionComponent->SetCollisionProfileName(TEXT("Projectile"));
+	RootComponent = CollisionComponent;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovement->UpdatedComponent = CollisionComponent;
+	ProjectileMovement->InitialSpeed = 3000.0f;
+	ProjectileMovement->MaxSpeed = 3000.0f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = false;
+
+	CollisionComponent->OnComponentHit.AddDynamic(this, &AHookProjectile::OnHit);
+}
+
+// Called when the game starts or when spawned
+void AHookProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+// Called every frame
+void AHookProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+void AHookProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// Пример логики при столкновении
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+
+	}
+}
---
+++ Source/doka3/HookProjectile.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HookProjectile.generated.h"
+
+UCLASS()
+class DOKA3_API AHookProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHookProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	class UProjectileMovementComponent* ProjectileMovement;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Collision")
+	class USphereComponent* CollisionComponent;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+};
